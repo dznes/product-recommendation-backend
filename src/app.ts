e3feb63ff74129000c68e068a6711808724320d1
dzns <davidkim1131@gmail.com>
2025-02-07 12:52:35 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import fastifyCors from '@fastify/cors'
+import { ZodError } from 'zod'
+import { env } from './env'
+import { AppRoutes } from './http/controllers/routes'
+
+// import { ProductionOrderRoutes } from './http/controllers/production-orders/routes'
+
+export const app = fastify()
+
+// Register @fastify/cors with options as needed
+app.register(fastifyCors, {
+  origin: [
+    '*',
+  ], // Allowed origins
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
+
+app.register(AppRoutes)
+
+
+// Ao adicionar o "_" na frente do parâmetro que não vai ser utilizado ou apenas o "_" no lugar do parâmetro, você informa que ele não vai ser utilizado.
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: `Validation error`, issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
